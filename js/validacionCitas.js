826c8f8156374f06ebbcfef8ad632c25dc47bca9
Brandon Alexis Machado <brandonmachado159@gmail.com>
2020-09-27 18:24:58 -0500

    se agrego valiacion a campos Nro ide de citas

--- js/validacionCitas.js
+++ js/validacionCitas.js
@@ -1,13 +1,38 @@
 
 function validacion(){
-    var tipCit = document.getElementById("TI");
 
+    
+    var tipCit = document.getElementById("TI");
+    
+
+    //verificacion de datos - tipo de cita
     if (tipCit.value === "") {
         alert("falta selecionar Tipo de cita");
         tipCit.style.border = "1px solid #f00";
         return false;
     }else{
         tipCit.style.border = "1px solid #0f0";
-        return true;
     }
+
+
+    var noIde = document.getElementById("NI");
+    var noIde2 = documen.getElementById("NI-2");
+    
+    //verificacion de datos - Nro Identificacion
+    if (noIde.value === "") {
+        alert("falta digitar Nro de identificacion");
+        noIde.style.border = "1px solid #f00";
+        return false;
+    }else{
+        noIde.style.border = "1px solid #0f0";
+    }
+
+    if (noIde2.value === "") {
+        alert("falta digitar Nro de identificacion");
+        noIde2.style.border = "1px solid #f00";
+        return false;
+    }else{
+        noIde2.style.border = "1px solid #0f0";
+    }
+
 }
