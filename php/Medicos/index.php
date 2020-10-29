7045cb6517c0220d28ee5eb952860964871b13b5
AntonyNarajo25 <antonynaranjo248@gmail.com>
2020-10-29 15:40:38 -0500

    index.php medicos

---
+++ php/Medicos/index.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en"> 
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+    <!-- Estilos Inicio CSS-->
+    <link rel="stylesheet" href="../css/styleInicio.css">
+
+    <title>IPS CECEP</title>
+  </head>
+  <body>
+    
+    
+  
+    <div class="text-center"> 
+    
+    <header id="header">
+      <video src="../video/full-video.mp4" autoplay loop muted>
+      </video>
+
+      <img  src="../img/ipslogo.png" id="logo">
+       
+
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="../html/inicio.html">Inicio </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../html/afiliados.html">Afiliados</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/empleados.html">Empleados</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/medicos.html">Medicos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/servicios.html">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/medicamentos.html">Medicamentos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/sedes.html">Sedes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/ciudades.html">Ciudades</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/paises.html">Paises</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../html/usuarios.html">Usuarios</a>
+            </li>
+           
+              
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+     
+    </div>
+ 
+    <!--Banner con efecto 3D-->
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" style="background-image:url(../img/banner1.png)"></div>
+        <div class="swiper-slide" style="background-image:url(../img/banner2.jpg)"></div>
+        <div class="swiper-slide" style="background-image:url(../img/banner3.jpg)"></div>
+        <div class="swiper-slide" style="background-image:url(../img/banner4.jpg)"></div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+    <!--Sesion de informacion de la IPS-->
+    <div class="informacion">
+      <div class="info">
+        <div class="parrafo-info">
+          <h3>Medicos</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae ex, et, eius adipisci molestias doloremque laborum sed optio reprehenderit ea aperiam quasi natus magnam! Aut dicta ratione dolorem maiores provident mollitia, magni eaque nesciunt, sapiente nobis cupiditate molestias debitis animi obcaecati perferendis. Incidunt repellendus a facilis sunt quibusdam laboriosam corrupti voluptatum illum quaerat, aliquam nihil libero, ex corporis, repudiandae debitis. Pariatur eaque, est molestias recusandae blanditiis omnis quis tenetur ex illum in consequatur quasi veritatis quidem impedit sint dicta facilis maiores. Laudantium sit iure minima veniam libero, distinctio eligendi praesentium ut harum, excepturi et itaque exercitationem provident, tempore tempora?</p>
+        </div>
+        <img src="../img/banner5.jpg" class="image-info">
+      </div>
+      <div class="info">
+        <div class="parrafo-info">
+          <h3>Odontologos</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae ex, et, eius adipisci molestias doloremque laborum sed optio reprehenderit ea aperiam quasi natus magnam! Aut dicta ratione dolorem maiores provident mollitia, magni eaque nesciunt, sapiente nobis cupiditate molestias debitis animi obcaecati perferendis. Incidunt repellendus a facilis sunt quibusdam laboriosam corrupti voluptatum illum quaerat, aliquam nihil libero, ex corporis, repudiandae debitis. Pariatur eaque, est molestias recusandae blanditiis omnis quis tenetur ex illum in consequatur quasi veritatis quidem impedit sint dicta facilis maiores. Laudantium sit iure minima veniam libero, distinctio eligendi praesentium ut harum, excepturi et itaque exercitationem provident, tempore tempora?</p>
+        </div>
+        <img src="../img/info-img (1).jpg" class="image-info">
+      </div>
+      <div class="info">
+        <div class="parrafo-info">
+          <h3>Psicologos</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae ex, et, eius adipisci molestias doloremque laborum sed optio reprehenderit ea aperiam quasi natus magnam! Aut dicta ratione dolorem maiores provident mollitia, magni eaque nesciunt, sapiente nobis cupiditate molestias debitis animi obcaecati perferendis. Incidunt repellendus a facilis sunt quibusdam laboriosam corrupti voluptatum illum quaerat, aliquam nihil libero, ex corporis, repudiandae debitis. Pariatur eaque, est molestias recusandae blanditiis omnis quis tenetur ex illum in consequatur quasi veritatis quidem impedit sint dicta facilis maiores. Laudantium sit iure minima veniam libero, distinctio eligendi praesentium ut harum, excepturi et itaque exercitationem provident, tempore tempora?</p>
+        </div>
+        <img src="../img/info-img (3).jpg" class="image-info">
+      </div>
+      <div class="info">
+        <div class="parrafo-info">
+          <h3>Hematologo</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae ex, et, eius adipisci molestias doloremque laborum sed optio reprehenderit ea aperiam quasi natus magnam! Aut dicta ratione dolorem maiores provident mollitia, magni eaque nesciunt, sapiente nobis cupiditate molestias debitis animi obcaecati perferendis. Incidunt repellendus a facilis sunt quibusdam laboriosam corrupti voluptatum illum quaerat, aliquam nihil libero, ex corporis, repudiandae debitis. Pariatur eaque, est molestias recusandae blanditiis omnis quis tenetur ex illum in consequatur quasi veritatis quidem impedit sint dicta facilis maiores. Laudantium sit iure minima veniam libero, distinctio eligendi praesentium ut harum, excepturi et itaque exercitationem provident, tempore tempora?</p>
+        </div>
+        <img src="../img/info-img (4).jpg" class="image-info">
+      </div>
+
+
+      <div class="contact-info">
+        <div class="targeta">
+          <i class="icon fas fa-envelope"></i>
+          <div class="contenido-targeta">
+            <h3>Correo</h3>
+            <span>ipsCecep@gmailcom</span>
+          </div>
+        </div>
+
+        <div class="targeta">
+          <i class="icon fas fa-phone"></i>
+          <div class="contenido-targeta">
+            <h3>Telefono</h3>
+            <span>+0084435</span>
+          </div>
+        </div>
+
+        <div class="targeta">
+          <i class="icon fas fa-map-marker-alt"></i>
+          <div class="contenido-targeta">
+            <h3>Ubicacion</h3>
+            <span>Colombia</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <footer class="footer" title="footer en progreso...">
+      
+    </footer>
+      
+    <script>
+      window.addEventListener('scroll', function(){
+        const header = document.querySelector('header');
+        header.classList.toggle("sticky", window.scrollY > 0);
+      })
+    </script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="../js/swiper.js"></script>
+  </body>
+</html>
