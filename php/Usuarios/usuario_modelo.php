f67ac9b5c9db8d5f802f2ae26f6e3e8a0355394e
jorgeivan0223 <jorgeivan0610@gmail.com>
2020-10-30 15:57:41 -0500

    z

--- php/Usuarios/usuario_modelo.php
+++ php/Usuarios/usuario_modelo.php
@@ -1,17 +1,18 @@
-  
 <?php
     require_once("../modeloAbstractoDB.php");
-    class Usuarios extends ModeloAbstractoDB {
-		private $id_usurio;
-		private $nom_usuario;
-        private $cedu_usuario;
-        private $usua_user;
+	
+    class Usuario extends ModeloAbstractoDB {
+        private $id_usuario;
+        private $nom_usuario;
+		private $cedu_usuario;
+		private $usua_user;
         private $celu_usuario;
         private $correo_usuario;
-        private $id_sede;
-        private $id_rol;
-        private $usua_pass;
+		private $id_sede ;
+		private $id_rol;
+		private $usua_pass;
 		private $usua_foto;
+	
 		
 		function __construct() {
 			//$this->db_name = '';
@@ -21,53 +22,57 @@
 			return $this->id_usuario;
 		}
 
-		public function getNom_usuario(){
+        
+        public function getNom_usuario(){
 			return $this->nom_usuario;
 		}
-		
+
 		public function getCedu_usuario(){
 			return $this->cedu_usuario;
-        }
-        
-        public function getUsua_user(){
+		}
+
+		public function getUsua_user(){
 			return $this->usua_user;
-        }
-        
-        public function getCelu_usuario(){
+		}
+		
+		public function getCelu_usuario(){
 			return $this->celu_usuario;
         }
         
         public function getCorreo_usuario(){
 			return $this->correo_usuario;
-        }
-        
-        public function getId_sede(){
+		}
+
+		public function getId_sede(){
 			return $this->id_sede;
-        }
-        
-        public function getId_rol(){
+		}
+		
+		public function getId_rol(){
 			return $this->id_rol;
         }
         
         public function getUsua_pass(){
 			return $this->usua_pass;
-        }
-        
-        public function getUsua_foto(){
-			return $this->usua_foto;
-        }
-        
-        
+		}
 
-		public function consultar($id_usuario='') {
-			if($id_usuario !=''):
-				$this->query = "
-				SELECT id_usuario, nom_usuario, cedu_usuario,usua_user,celu_usuario, correo_usuario, id_sede,usua_pass,usua_foto
-				FROM tb_usuarios
-				WHERE id_usuario = '$id_usuario' order by id_usuario
-				";
-				$this->obtener_resultados_query(); // se ejecuta cuando sea consulta select 
-			endif;
+		public function getUsua_foto(){
+			return $this->usua_foto;
+		}
+
+
+		
+		public function consultar($datos = array()) {
+			
+			$usuario = $datos['usuario'];
+			$password = $datos['password'];
+            $this->query = "
+            SELECT id_usuario, usua_user, usua_pass, nom_usuario, usua_foto
+			FROM tb_usuarios 
+			WHERE usua_user = '$usuario'
+			";
+
+            $this->obtener_resultados_query();
+			
 			if(count($this->rows) == 1):
 				foreach ($this->rows[0] as $propiedad=>$valor):
 					$this->$propiedad = $valor;
@@ -77,60 +82,63 @@
 		
 		public function lista() {
 			$this->query = "
-			SELECT id_usuario, nom_usuario, cedu_usuario,usua_user,celu_usuario, correo_usuario, id_sede,usua_pass,usua_foto
-			FROM tb_usuarios as c inner join tb_sedes as m
-			ON (c.id_sede = m.id_sede) order by id_medico
+			SELECT comu_codi, comu_nomb, m.muni_nomb
+			FROM tb_comuna as c inner join tb_municipio as m
+			ON (c.muni_codi = m.muni_codi) order by comu_codi
 			";
 			
 			$this->obtener_resultados_query();
 			return $this->rows;
 			
 		}
-		
+        
+        public function generarPassword($pass=""){
+            $opciones = [
+                'cost' => 12,
+            ];
+            
+            $passwordHashed = password_hash($pass, PASSWORD_BCRYPT, $opciones);
+            
+            return $passwordHashed;
+        }
+
 		public function nuevo($datos=array()) {
-			if(array_key_exists('id_usuario', $datos)):
+			if(array_key_exists('usua_codi', $datos)):
 				foreach ($datos as $campo=>$valor):
 					$$campo = $valor;
-				endforeach;
-				$nom_usuario= utf8_decode($nom_medico);
+                endforeach;
+              
 				$this->query = "
 					INSERT INTO tb_usuario
-					(id_usuario, nom_usuario, cedu_emplado, celu_usuario dire_usuario email_usuario id_sede)
+					(usua_codi, usua_user, usua_nomb, usua_pass,perso_cod,update_at)
 					VALUES
-					(NULL, '$nom_usuario, '$cedu_emplado', '$celu_usuario, '$dire_usuario, '$email_usuario, '$id_sede')
+					(NULL, '$comu_nomb', '$muni_codi',NOW())
 					";
-				$resultado = $this->ejecutar_query_simple();
-				return $resultado;
+					$resultado = $this->ejecutar_query_simple();
+					return $resultado;
 			endif;
+			
 		}
 		
 		public function editar($datos=array()) {
 			foreach ($datos as $campo=>$valor):
 				$$campo = $valor;
 			endforeach;
-			$nom_usuario utf8_decode($nom_usuario);
 			$this->query = "
-			UPDATE tb_usuarios
-		SET id_usuario'$id_usurio';
-		nom_usuario'$nom_usuario';
-        cedu_usuario '$cedu_usuario';
-        usua_user '$usua_user';
-        celu_usuario '$celu_usuario';
-        correo_usuario '$correo_usuario';
-        id_sede '$id_sede';
-        id_rol '$id_rol';
-        usua_pass '$usua_pass';
-		usua_foto '$usua_foto';
-
+			UPDATE tb_comuna
+			SET comu_nomb ='$comu_nomb',
+			muni_codi ='$muni_codi',
+			update_at = NOW()
+			WHERE comu_codi = '$comu_codi'
 			";
 			$resultado = $this->ejecutar_query_simple();
 			return $resultado;
 		}
 		
-		public function borrar($id_usuario'') {
+		public function borrar($comu_codi='') {
 			$this->query = "
-			DELETE FROM tb_usuario
-			WHERE id_usuario= '$id_usuario
+			DELETE FROM tb_comuna
+			WHERE comu_codi = '$comu_codi'
 			";
 			$resultado = $this->ejecutar_query_simple();
 
