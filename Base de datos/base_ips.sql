180665d240a601c44b75261595d36136995ebe1e
DeivyRivas <deyruiz555@gmail.com>
2020-10-30 23:26:22 -0500

    por favor importar la new BD

--- Base de datos/base_ips.sql
+++ Base de datos/base_ips.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-10-2020 a las 15:45:19
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.6.15
+-- Tiempo de generación: 31-10-2020 a las 05:23:57
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -85,8 +86,9 @@ CREATE TABLE `tb_ciudades` (
 
 INSERT INTO `tb_ciudades` (`id_ciudad`, `nom_ciudad`, `id_pais`) VALUES
 (1, 'Cali', 1),
-(2, 'Bogota', 1),
-(3, 'Buenos Aires', 2);
+(2, 'la concha', 2),
+(3, 'Buenos Aires', 2),
+(4, 'Pereira', 1);
 
 -- --------------------------------------------------------
 
@@ -109,18 +111,17 @@ CREATE TABLE `tb_empleados` (
 --
 
 INSERT INTO `tb_empleados` (`id_empleado`, `nom_empleado`, `cedu_emplado`, `celu_empleado`, `dire_empleado`, `email_empleado`, `id_sede`) VALUES
-(2, 'arley rivas', 96766524, 143345, '', 'arley@gmail.com', 5),
-(4, 'Deivy Rivas', 1143997886, 313765146, '', 'deyruiz555qgmail.com', 3),
-(5, 'Diana', 773464288, 310726545, '', 'dianaRia@hotmail.com', 1),
+(2, 'arley ', 96764444, 140000, '', 'asprillarivas@gmail.com', 3),
+(4, 'Deivy Rivas Steven ', 1143997886, 313765146, '', 'deyruiz555qgmail.com', 3),
 (6, 'paulina Roa', 55426754, 32352823, '', 'pau@hotmail.com', 6),
-(7, 'Yeferson ', 6345223, 31076347, '', 'yeriru@gmail.com', 6),
+(7, 'Yeferson  rios', 6345223, 31076347, '', 'yeriru@gmail.com', 6),
 (8, 'pedro', 1113123, 11245, 'calle 6 - 54N', 'pedrito@gmail.com', 1),
-(9, 'pedro A', 1113123, 112455, '', 'pedrito@gmail.com', 4),
-(10, 'pedroo', 1113123, 11245, 'calle 6', 'pedrito@gmail.com', 1),
-(11, 'Diana maria rivas', 773464288, 310726545, 'carrera 82 # 1 - 5 ', 'diana123@hotmail.com', 5),
-(12, 'deivy', 1143997888, 313765146, 'cra 24d # 42a - 07', 'deyruiz555qgmail.com', 3),
-(13, 'pedroo', 888888888, 11245, 'rodeo', 'pedrito@gmail.com', 3),
-(14, 'pedroo', 88888, 11245, 'rodeo', 'pedrito@gmail.com', 3);
+(12, 'deivy ruiz', 1143997888, 313765146, '', 'deyruiz555qgmail.com', 3),
+(13, 'pedroo', 2323232, 453412, '', 'pedrito@gmail.com', 5),
+(16, 'mauricio', 2345212, 31397463, 'cra 2 # 4 -0', 'mau@gmail.co', 5),
+(17, 'pepito peres', 11111111, 986345, 'ave 32 # 21- 76', 'pepo@hotmail.com', 5),
+(18, 'alberto', 6537524, 31298356, 'cra 5 - 4', 'alber@gmail.com.co', 6),
+(20, 'Arrinann', 23243241, 321432423, 'cra 23 - 34', 'ari@gmail.com.co', 5);
 
 -- --------------------------------------------------------
 
@@ -130,20 +131,24 @@ INSERT INTO `tb_empleados` (`id_empleado`, `nom_empleado`, `cedu_emplado`, `celu
 
 CREATE TABLE `tb_medicamentos` (
   `id_medicamento` int(11) NOT NULL,
-  `nom_medicamento` varchar(255) NOT NULL,
-  `fecha_venci` date NOT NULL
+  `nom_medicamento` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tb_medicamentos`
 --
 
-INSERT INTO `tb_medicamentos` (`id_medicamento`, `nom_medicamento`, `fecha_venci`) VALUES
-(1, 'Acetaminofen', '2023-01-20'),
-(2, 'Dolex', '2023-01-20'),
-(3, 'Asitromicina', '2023-01-20'),
-(4, 'Lebofloxacina', '2023-01-20'),
-(5, 'Noxpirin', '2023-01-20');
+INSERT INTO `tb_medicamentos` (`id_medicamento`, `nom_medicamento`) VALUES
+(1, 'Acetaminofen'),
+(2, 'Dolex'),
+(3, 'Asitromicina'),
+(4, 'Lebofloxacina'),
+(5, 'Noxpirin'),
+(6, 'Aspirinitas'),
+(7, 'Advil'),
+(9, 'Diclofenaco'),
+(10, 'Ibuprofeno'),
+(12, 'alcohol');
 
 -- --------------------------------------------------------
 
@@ -166,10 +171,9 @@ CREATE TABLE `tb_medicos` (
 --
 
 INSERT INTO `tb_medicos` (`id_medico`, `nom_medico`, `celu_medico`, `Especialista`, `cedu_medico`, `correo_medico`, `id_sede`) VALUES
-(1, 'pedro santiago', 31934576, 'Medico General', 12345, 'pedro@gmail.com', 1),
-(2, 'santiago reos', 3165476, 'Psicologo', 987345, 'reos@gmail.com', 1),
-(3, 'maria jose', 3123476, 'Odontologa', 78645, 'mariajose@gmail.com', 1),
-(4, 'antony naranjo', 31734642, 'Cirujano', 1010104106, 'antony@gmail.com', 3);
+(1, 'pedro santiago', 31934576, 'cirujano', 12345, 'pedro@gmail.com', 1),
+(2, 'santiago reos', 3165476, 'medico general', 987345, 'reos@gmail.com', 1),
+(3, 'ana jose', 3123476, 'Cirujano', 78645, 'ana@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -188,7 +192,13 @@ CREATE TABLE `tb_pais` (
 
 INSERT INTO `tb_pais` (`id_pais`, `nom_pais`) VALUES
 (1, 'Colombia'),
-(2, 'Argentina');
+(2, 'Argentina'),
+(5, 'Brasil'),
+(6, 'Paraguay'),
+(7, 'Peru'),
+(8, 'Uruguay'),
+(9, 'Honduras'),
+(11, 'Holanda');
 
 -- --------------------------------------------------------
 
@@ -235,7 +245,7 @@ INSERT INTO `tb_sedes` (`id_sede`, `nom_sede`, `direc_sede`, `tele_sede`, `id_ci
 (2, 'sanitas', 'av 6n # 36n - 7', 834564, 1),
 (3, 'Diosito', 'trnasv 6s # 33n - 67', 786436, 1),
 (4, 'san juan', 'calle 6 # 45 - 47', 112342, 1),
-(5, 'cruz roja', 'calle 24d # 42a - 07', 112312, 1),
+(5, 'cruz roja - rojita', 'calle 24d # 42a - 07', 112312, 2),
 (6, 'cruz verde', 'av 5n # 2 - 12 ', 23423, 1);
 
 -- --------------------------------------------------------
@@ -256,11 +266,10 @@ CREATE TABLE `tb_servicio` (
 
 INSERT INTO `tb_servicio` (`id_servicio`, `tipo_servicio`, `id_sede`) VALUES
 (1, 'Cita con medico general', 1),
-(2, 'Cita con medico general', 1),
+(2, 'Cita con medico general', 2),
 (3, 'Especialista', 1),
 (4, 'Laboratorios Clinicos', 1),
 (5, 'Odontologia', 1),
-(6, 'Cita con medico general', 2),
 (7, 'Especialista', 2),
 (8, 'Laboratorios Clinicos', 2),
 (9, 'Odontologia', 2);
@@ -290,7 +299,8 @@ CREATE TABLE `tb_usuarios` (
 
 INSERT INTO `tb_usuarios` (`id_usuario`, `nom_usuario`, `cedu_usuario`, `usua_user`, `celu_usuario`, `correo_usuario`, `id_sede`, `id_rol`, `usua_pass`, `usua_foto`) VALUES
 (4, 'Deivi Steven Rivas Rivas', 223451, 'deivy', 53453424, 'Drivas@gmail.com.co', 1, 1, '$2y$12$xyMXafB//kPIBlb6kBjUUe9ojYPpLgWT.uKEFdRMDfEzp7YffmmvW', 'deivy.jpg'),
-(7, 'Pedro Bustamante Arqueo', 5562543, 'pedro', 31245673, 'Pedro@otro.com.co', 2, 3, '$2y$12$c0SuyCU6hdqKMf7RcW/ya.u0JfVRNcY8M10L7Thb2yvn0DaQT5w/a', 'avatar.png');
+(7, 'Pedro Bustamante Arqueo', 5562543, 'pedro', 31245673, 'Pedro@otro.com.co', 2, 3, '$2y$12$c0SuyCU6hdqKMf7RcW/ya.u0JfVRNcY8M10L7Thb2yvn0DaQT5w/a', 'avatar.png'),
+(8, 'pepa pina peres', 223312, 'pepa', 3217465, 'pepa@gmail.com', 2, 2, '$2y$12$Ubr7V9MdvGbGXtMZOgR48uTnnEEQX.ewieAw239W4UEgrVNftuq16', 'user2-160x160');
 
 --
 -- Índices para tablas volcadas
@@ -380,56 +390,67 @@ ALTER TABLE `tb_usuarios`
 --
 ALTER TABLE `tb_afiliados`
   MODIFY `id_afiliado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_citas`
 --
 ALTER TABLE `tb_citas`
   MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_ciudades`
 --
 ALTER TABLE `tb_ciudades`
-  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_ciudad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_empleados`
 --
 ALTER TABLE `tb_empleados`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_medicamentos`
 --
 ALTER TABLE `tb_medicamentos`
-  MODIFY `id_medicamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_medicamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_medicos`
 --
 ALTER TABLE `tb_medicos`
-  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_pais`
 --
 ALTER TABLE `tb_pais`
-  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_roles`
 --
 ALTER TABLE `tb_roles`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_sedes`
 --
 ALTER TABLE `tb_sedes`
-  MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_servicio`
 --
 ALTER TABLE `tb_servicio`
   MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -482,6 +503,7 @@ ALTER TABLE `tb_servicio`
 ALTER TABLE `tb_usuarios`
   ADD CONSTRAINT `tb_usuarios_ibfk_1` FOREIGN KEY (`id_sede`) REFERENCES `tb_sedes` (`id_sede`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `tb_usuarios_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `tb_roles` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
