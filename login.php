945d0d91926eea12657f983408b4f2db34103fef
Brandon Alexis Machado <brandonmachado159@gmail.com>
2020-11-04 19:15:08 -0500

    se elimino fondo de login para iniciar la modificacion completa de la interface

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body background="Recursos/img/medicamento.jpg">
+<body>
 
 <div class="login-box">
   <div class="login-logo">
