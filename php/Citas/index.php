a57e1ad95085d1604a449b46fb1db55c3db14998
DeivyRivas <deyruiz555@gmail.com>
2020-11-16 12:21:32 -0500

    se agrega boton en formulario citas para visializar el reporte

--- php/Citas/index.php
+++ php/Citas/index.php
@@ -20,6 +20,9 @@
               <div class="box-tools pull-right">
                   <button class="btn btn-info btn-sm" id="nuevo"  data-toggle="tooltip" 
                       title="Nueva cita"><i class="fa fa-plus" aria-hidden="true"></i></button> 
+                      
+                      <a href="./reportes/Reports/reporteCitas.php" class="btn btn-info">reportes</a>
+                       
               </div>
             </div>
            
