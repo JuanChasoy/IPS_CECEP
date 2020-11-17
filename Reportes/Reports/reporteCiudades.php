acb39a9e504760927d1dfbca882f6be85e235a2e
Brandon Alexis <>
2020-11-17 17:43:40 -0500

    se cambiaron colores, estructura, consulta para 'Ciudades'

--- Reportes/Reports/reporteCiudades.php
+++ Reportes/Reports/reporteCiudades.php
@@ -26,13 +26,13 @@ function Header()
 
     // Encabezados de tablas
     $this->SetTextColor(255, 255, 255);
-    $this->SetFillColor(107, 214, 235 );
+    $this->SetFillColor(22, 171, 236);
     $this->SetFontSize(15);
     $this->SetDrawColor(0);
     $this->SetLineWidth(0);
-    $this->Cell(50, 10, 'Id_Ciudad', 1, 0, 'C', 1);
-    $this->Cell(50, 10, 'Ciudad', 1, 0, 'C', 1);
-    $this->Cell(50, 10, 'Pais', 1, 0, 'C', 1);
+    $this->Cell(25, 10, 'Id', 1, 0, 'C', 1);
+    $this->Cell(80, 10, 'Ciudad', 1, 0, 'C', 1);
+    $this->Cell(85, 10, 'Pais', 1, 0, 'C', 1);
     $this->Ln();
 }
 
@@ -46,12 +46,16 @@ function Footer()
     $this->SetTextColor(255,255,225);
     $this->SetFillColor(12, 143, 200);
     // Número de página    
-    $this->Cell(0,10, utf8_decode('Página ').$this->PageNo().'-{nb}',0,0,'C', 1);
+    $this->Cell(0,10, utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C', 1);
 }
 }
 
 #$consulta = "SELECT nom_pais FROM tb_pais";
-$consulta = "SELECT * FROM tb_ciudades";
+//$consulta = "SELECT * FROM tb_ciudades";
+$consulta = "SELECT id_ciudad, nom_ciudad, p.nom_pais
+            FROM tb_ciudades AS c
+            INNER JOIN tb_pais AS p
+            ON (c.id_pais = p.id_pais)";
 $resultado = $mysqli->query($consulta);
 // Creación del objeto de la clase heredada
 $pdf = new PDF();
@@ -63,14 +67,14 @@ $fill = false;
 while($row = $resultado->fetch_assoc()){
     if($fill){
         $fill = !$fill;
-        $pdf->SetFillColor(224,235,255);
+        $pdf->SetFillColor(203, 236, 245);
     }else{
         $fill = !$fill;
-        $pdf->SetFillColor(255, 255, 255);
+        $pdf->SetFillColor(76, 89, 92);
     }
-    $pdf->Cell(50, 10, $row['id_ciudad'], 1, 0, 'C', 1);
-    $pdf->Cell(50, 10, $row['nom_ciudad'], 1, 0, 'C', 1);
-    $pdf->Cell(50, 10, $row['id_pais'], 1, 0, 'C', 1);
+    $pdf->Cell(25, 10, $row['id_ciudad'], 1, 0, 'C', 1);
+    $pdf->Cell(80, 10, $row['nom_ciudad'], 1, 0, 'C', 1);
+    $pdf->Cell(85, 10, $row['nom_pais'], 1, 0, 'C', 1);
     $pdf->Ln();
     
 }
