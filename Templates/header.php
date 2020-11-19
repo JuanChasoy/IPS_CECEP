b94dd9bfc23d70425f0579b46b40c9db1f46d05b
Brandon Alexis <>
2020-11-19 10:55:04 -0500

    se agrego enlace css a header para la apariencia de los botones de reportes en General

--- Templates/header.php
+++ Templates/header.php
@@ -20,6 +20,7 @@
 
   <link rel="stylesheet" href="./Recursos/css/admin.css">
 
+  <link rel="stylesheet" href="./css/styleButtons.css">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
