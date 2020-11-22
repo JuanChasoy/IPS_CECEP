ba75237b4e04307641195a5c2aaad8ad20020f0e
JuanChasoy <jchasoy1100mail.com>
2020-11-21 19:07:02 -0500

    CREANDO USUARIO CLIENTE, Y HACIENDO BUSQUEDAS

---
+++ Templates/Cliente/navegacion.php
@@ -0,0 +1,58 @@
+<?php include_once ("./Funciones/sessiones.php"); ?>
+<!-- Left side column. contains the sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+        <img src="./Recursos/img/<?php echo $_SESSION['foto'] ?>" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p><?php echo $_SESSION["nombre"]; ?></p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Activo</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MENÚ DE CLIENTE</li>
+          <li class="treeview">          
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-circle-o"></i> Nada disponible </a></li>
+            </ul>
+          </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-cogs"></i>
+            <span>Menu</span>
+          </a>
+          <ul class="treeview-menu">
+           
+            <li><a href="./php/Medicamentos/index.php"><i class="fas fa-globe-americas"></i> Medicamentos</a></li>            
+           <!-- <li><a href="./php/Afiliados/index.php"><i class="fab fa-accessible-icon"></i> Afiliados</a></li> -->
+            <li><a href="./php/Citas/index.php"><i class="fab fa-accessible-icon"></i> Citas</a></li>
+           <!--  <li><a href="./php/Usuarios/index.php"><i class="fab fa-accessible-icon"></i> Usuarios</a></li> -->
+            <li><a href="./php/Medicos/index.php"><i class="fas fa-globe-americas"></i> Medicos</a></li> 
+            <li><a href="./php/Paises/index.php"><i class="fas fa-globe-americas"></i> Paises</a></li> 
+            <li><a href="./php/Ciudades/index.php"><i class="fas fa-globe-americas"></i> Ciudades</a></li> 
+            <li><a href="./php/Sedes/index.php"><i class="fas fa-globe-americas"></i> Sedes</a></li> 
+            <li><a href="./php/Servicios/index.php"><i class="fas fa-globe-americas"></i> Servicios</a></li> 
+          </ul>
+        </li>
+       
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- =============================================== -->
