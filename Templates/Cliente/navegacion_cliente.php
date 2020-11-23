80f4d6c7df505dd002c93aff6a1a952e377d3146
JuanChasoy <jchasoy1100@gmail.com>
2020-11-23 14:14:01 -0500

    elimine y cambie el ingreso del cliente

--- Templates/Cliente/navegacion_cliente.php
+++ Templates/Cliente/navegacion_cliente.php
@@ -26,7 +26,7 @@
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MENÚ DE CLIENTE</li>
+        <li class="header">MENÚ DE CLIENTES</li>
           <li class="treeview">          
             <ul class="treeview-menu">
               <li><a href="#"><i class="fa fa-circle-o"></i> Nada disponible </a></li>
@@ -39,15 +39,10 @@
           </a>
           <ul class="treeview-menu">
            
-            <li><a href="./Templates/Cliente/Medicamentos/index.php"><i class="fas fa-globe-americas"></i> Medicamentos</a></li>            
-            <li><a href="./Templates/Cliente/Afiliados/index.php"><i class="fab fa-accessible-icon"></i> Afiliados</a></li> 
-            <li><a href="./Templates/Cliente/Citas/index.php"><i class="fab fa-accessible-icon"></i> Citas</a></li>
-            <li><a href="./Templates/Cliente/Usuarios/index.php"><i class="fab fa-accessible-icon"></i> Usuarios</a></li> 
-            <li><a href="./Templates/Cliente/Medicos/index.php"><i class="fas fa-globe-americas"></i> Medicos</a></li> 
-            <li><a href="./Templates/Cliente/Paises/index.php"><i class="fas fa-globe-americas"></i> Paises</a></li> 
-            <li><a href="./Templates/Cliente/Ciudades/index.php"><i class="fas fa-globe-americas"></i> Ciudades</a></li> 
-            <li><a href="./Templates/Cliente/Sedes/index.php"><i class="fas fa-globe-americas"></i> Sedes</a></li>
-            <li><a href="./Templates/Cliente/Servicios/index.php"><i class="fas fa-globe-americas"></i> Servicios</a></li> 
+            <li><a href="./php/Medicamentos/index.php"><i class="fas fa-globe-americas"></i> Medicamentos</a></li>            
+            <li><a href="./php/Afiliados/index.php"><i class="fab fa-accessible-icon"></i> Afiliados</a></li>
+            <li><a href="./php/Citas/index.php"><i class="fab fa-accessible-icon"></i> Citas</a></li>
+            <li><a href="./php/Usuarios/index.php"><i class="fab fa-accessible-icon"></i> Usuarios</a></li>
           </ul>
         </li>
        
